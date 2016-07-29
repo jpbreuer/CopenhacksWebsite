30710cb5398d1abdfdcd7f0b04bf15a68be85d17
jpbreuer <jeanpaul.breuer@gmail.com>
2016-07-29 03:52:05 -0400

    Complete overhaul, added photostream

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name copenHacksAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the copenHacksAngularApp
+ */
+angular.module('copenHacksAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
